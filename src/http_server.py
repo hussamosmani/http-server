2cd7814ceeba22ba47212b28159f3b65ac0649b9
unknown <hussam123-lab>
2025-06-10 20:56:55 +1000

    added support to allow for dynmic instantiation of objects for functions

--- src/http_server.py
+++ src/http_server.py
@@ -1,3 +1,7 @@
+import inspect
+import json
+
+from pydantic.main import ModelMetaclass
 from src.exceptions.http_exception import HTTPMethodException
 from src.tcp_server import TCPServer
 from src.models.router_trie import RouterTrie
@@ -20,18 +24,21 @@ class HTTPServer(TCPServer):
         return wrapper
 
     def handle_request(self, data: str):
-        method, request_target, protocol = data.split()
+        request = data.split("\r\n")
+        method, request_target, protocol = request[0].split(" ")
+        data = json.loads(request[2]) if len(request) > 2 else None
         if method == HTTPMethod.GET.value:
-            self._handle_get(request_target=request_target)
+            self._handle_request(request_target=request_target)
 
         elif method == HTTPMethod.POST.value:
-            pass
+            self._handle_request(request_target=request_target, data=data)
+
         elif method == HTTPMethod.PUT.value:
             pass
         else:
             raise HTTPMethodException(method)
 
-    def _handle_get(self, request_target: str):
+    def _handle_request(self, request_target: str, data: any = None):
         found_handler_and_args = self.routes_trie.search(
             path=request_target, method_type=HTTPMethod.GET
         )
@@ -39,6 +46,13 @@ class HTTPServer(TCPServer):
             return
         handler, args = found_handler_and_args
 
-        handler(**args)
+        request = None
+        for parameter, annotation in handler.__annotations__.items():
+            if isinstance(annotation, ModelMetaclass):
+                request = {parameter: annotation(**data)}
+
+        combined_args = {**args, **request} if data and request else args
+
+        handler(**combined_args)
 
         exit()
